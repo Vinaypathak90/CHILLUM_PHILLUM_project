411bc16a9c9145e8aa6ade341e64ed55143b9108
researchbuimb24-spec <researchbuimb24@gmail.com>
2026-04-06 12:21:02 +0530

    fix timeoutv2

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -111,16 +111,18 @@ const sendOTPEmail = async (email, otpCode) => {
     console.log("-----------------------------------------\n");
 
     try {
-        const transporter = nodemailer.createTransport({
-            service: 'smtp.gmail.com',
+       const transporter = nodemailer.createTransport({
+            service: 'gmail', // Double safety
+            host: 'smtp.gmail.com',
             port: 465,
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
             },
             tls: {
-        rejectUnauthorized: false 
-    }
+                rejectUnauthorized: false 
+            }
         });
 
         const mailOptions = {
