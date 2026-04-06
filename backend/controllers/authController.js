dfb6f72b587c438baec630926f69aa6be3b402bc
researchbuimb24-spec <researchbuimb24@gmail.com>
2026-04-06 12:37:52 +0530

    Try Port 587

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -111,11 +111,17 @@ const sendOTPEmail = async (email, otpCode) => {
     console.log("-----------------------------------------\n");
 
     try {
-      const transporter = nodemailer.createTransport({
-            service: 'gmail',
+       const transporter = nodemailer.createTransport({
+          //  service: 'gmail', // Double safety
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
+            },
+            tls: {
+                rejectUnauthorized: false 
             }
         });
 
