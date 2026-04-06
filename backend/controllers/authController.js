886a2f75371c3d493f6043db72f7a68440eadf1e
researchbuimb24-spec <researchbuimb24@gmail.com>
2026-04-06 14:19:38 +0530

    otp

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -112,16 +112,16 @@ const sendOTPEmail = async (email, otpCode) => {
 
     try {
        const transporter = nodemailer.createTransport({
-          //  service: 'gmail', // Double safety
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: true,
+            service: 'gmail', // Double safety
+            
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
             },
             tls: {
-                rejectUnauthorized: false 
+                rejectUnauthorized: false,
+                minVersion: 'TLSv1.2'
+
             }
         });
 
